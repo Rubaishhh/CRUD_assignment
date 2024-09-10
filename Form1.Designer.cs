3cae79f08c71f3775ccb9aa587646c669235a722
Rubaiyat Islam <rishanto108@gmail.com>
2024-09-10 23:45:57 +0600

    CRUD application

---
+++ Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace CRUD_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_uname = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmb_gen = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_name
+            // 
+            this.txt_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txt_name.Location = new System.Drawing.Point(206, 203);
+            this.txt_name.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txt_name.Multiline = true;
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(266, 40);
+            this.txt_name.TabIndex = 0;
+            this.txt_name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_uname
+            // 
+            this.txt_uname.Location = new System.Drawing.Point(206, 143);
+            this.txt_uname.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txt_uname.Multiline = true;
+            this.txt_uname.Name = "txt_uname";
+            this.txt_uname.Size = new System.Drawing.Size(266, 39);
+            this.txt_uname.TabIndex = 0;
+            this.txt_uname.TextChanged += new System.EventHandler(this.txt_uname_TextChanged);
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pass.ForeColor = System.Drawing.Color.Black;
+            this.txt_pass.Location = new System.Drawing.Point(206, 273);
+            this.txt_pass.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txt_pass.Multiline = true;
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(266, 35);
+            this.txt_pass.TabIndex = 1;
+            this.txt_pass.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(44, 203);
+            this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(72, 25);
+            this.label.TabIndex = 2;
+            this.label.Text = "Name";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 273);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.BackColor = System.Drawing.Color.Transparent;
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(44, 141);
+            this.username.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(90, 25);
+            this.username.TabIndex = 3;
+            this.username.Text = "User ID";
+            this.username.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.username.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 410);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(298, 410);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(392, 410);
+            this.save_btn.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(74, 36);
+            this.save_btn.TabIndex = 4;
+            this.save_btn.Text = "Insert";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 348);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Stencil", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(200, 2);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 44);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "DataBase";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(549, 84);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(574, 483);
+            this.panel1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 408);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(484, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(57, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(484, 313);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // cmb_gen
+            // 
+            this.cmb_gen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_gen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_gen.FormattingEnabled = true;
+            this.cmb_gen.Items.AddRange(new object[] {
+            "",
+            "Male",
+            "Female",
+            "Others"});
+            this.cmb_gen.Location = new System.Drawing.Point(206, 348);
+            this.cmb_gen.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.cmb_gen.Name = "cmb_gen";
+            this.cmb_gen.Size = new System.Drawing.Size(266, 33);
+            this.cmb_gen.TabIndex = 8;
+            this.cmb_gen.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(432, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 45);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "CRUD Application";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CRUD_Application.Properties.Resources.file;
+            this.ClientSize = new System.Drawing.Size(1215, 646);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmb_gen);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_uname);
+            this.Controls.Add(this.txt_name);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_uname;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmb_gen;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CRUD_Application
+{
+    public partial class Form1 : Form
+    {
+        string id;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection con = new SqlConnection("Data Source=NEMESIS\\SQLEXPRESS;Initial Catalog=CRUD_app;Integrated Security=True;Encrypt=False");
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //input validation
+            if (string.IsNullOrWhiteSpace(txt_pass.Text) ||
+                string.IsNullOrWhiteSpace(txt_uname.Text) ||
+                cmb_gen.SelectedItem == null)
+            {
+                //Show error message if any field is empty or gender is not selected
+                MessageBox.Show("Please fill in all fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                con.Open();
+
+                
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO user_info (name, password, userid, gender) VALUES ('"
+                    + txt_name.Text + "', '"
+                    + txt_pass.Text + "', '"
+                    + txt_uname.Text + "', '"
+                    + cmb_gen.SelectedItem.ToString() + "')", con);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Registration successful!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                //Ensure the connection is closed
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+
+            // refresh data
+            BindData();
+        }
+
+        void BindData()
+        {
+
+            SqlCommand cmd = new SqlCommand("select * from user_info", con);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            if(dt.Rows.Count > 0 )
+                dataGridView1.DataSource = dt;
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update user_info set name = '"+ txt_name.Text + "',password = '" + txt_pass.Text + "',userid = '" + txt_uname.Text + "',gender = '" + cmb_gen.SelectedItem.ToString() + "' where userid = '" + id + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            BindData();
+            MessageBox.Show("Successfully updated!!!");
+            
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            txt_name.Text = "";
+            txt_pass.Text = "";
+            txt_uname.Text = "";
+            cmb_gen.SelectedIndex = 0;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from user_info where userid = '"+id+"'",con);
+            cmd.ExecuteNonQuery();
+            BindData();
+            txt_name.Text = "";
+            txt_pass.Text = "";
+            txt_uname.Text = "";
+            MessageBox.Show("User Deleted!!");
+            con.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void txt_uname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                dataGridView1.CurrentRow.Selected = true;
+                txt_name.Text = dataGridView1.Rows[e.RowIndex].Cells["name"].Value.ToString();
+                txt_pass.Text = dataGridView1.Rows[e.RowIndex].Cells["password"].Value.ToString();
+                txt_uname.Text = dataGridView1.Rows[e.RowIndex].Cells["userid"].Value.ToString();
+                cmb_gen.Text = dataGridView1.Rows[e.RowIndex].Cells["gender"].Value.ToString();
+                id = dataGridView1.Rows[e.RowIndex].Cells["userid"].Value.ToString();
+            }
+
+        }
+    }
+}
